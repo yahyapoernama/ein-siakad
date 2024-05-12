52c01e662dffb2412b8f18cb994a3c51aafc8d81
Mochamad Yahya Dwi Purnama <yahyapoernama@gmail.com>
2024-05-12 10:51:20 +0700

    feat: Add Column (Username and Role) and Seeder

---
+++ database/migrations/2024_03_04_085723_alter_users_add_column_username_and_role.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->nullable()->after('name');
+            $table->enum('role', ['Superadmin', 'Admin', 'User'])->default('User')->after('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('username');
+            $table->dropColumn('role');
+        });
+    }
+};
