f12e7d1bcca2e838400dc5d9eb53e209af6763c9
Mochamad Yahya Dwi Purnama <yahyapoernama@gmail.com>
2024-05-15 00:00:11 +0700

    style: Customize Dashboard Content

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,64 +11,15 @@
 
 @section('content')
 <div class="row">
-    <div class="col-xl-6 col-xxl-5 d-flex">
+    <div class="col-xl-12 col-xxl-12 d-flex">
         <div class="w-100">
             <div class="row">
-                <div class="col-sm-6">
+                <div class="col-sm-12">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title mb-4">Sales</h5>
-                            <h1 class="display-5 mt-1 mb-3">2.382</h1>
-                            <div class="mb-1">
-                                <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -3.65% </span>
-                                <span class="text-muted">Since last week</span>
-                            </div>
+                            Selamat Datang  <b>{{ auth()->user()->name }}</b>!
                         </div>
                     </div>
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title mb-4">Visitors</h5>
-                            <h1 class="display-5 mt-1 mb-3">14.212</h1>
-                            <div class="mb-1">
-                                <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 5.25% </span>
-                                <span class="text-muted">Since last week</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title mb-4">Earnings</h5>
-                            <h1 class="display-5 mt-1 mb-3">$21.300</h1>
-                            <div class="mb-1">
-                                <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 6.65% </span>
-                                <span class="text-muted">Since last week</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title mb-4">Orders</h5>
-                            <h1 class="display-5 mt-1 mb-3">64</h1>
-                            <div class="mb-1">
-                                <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -2.25% </span>
-                                <span class="text-muted">Since last week</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-6 col-xxl-7">
-        <div class="card flex-fill w-100">
-            <div class="card-header">
-                <h5 class="card-title mb-0">Recent Movement</h5>
-            </div>
-            <div class="card-body py-3">
-                <div class="chart chart-sm">
-                    <canvas id="chartjs-dashboard-line"></canvas>
                 </div>
             </div>
         </div>
