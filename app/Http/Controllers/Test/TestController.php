2de05c14e83345c7132486a1441bff950c739912
yahyapoernama <yahyapoernama@gmail.com>
2024-08-02 15:12:46 +0700

    test: Add TestController

---
+++ app/Http/Controllers/Test/TestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Test;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function test1()
+    {
+        return response(200);
+    }
+}
